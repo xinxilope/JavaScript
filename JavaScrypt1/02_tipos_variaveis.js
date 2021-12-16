d44988a9906b561a8e75f13f04d763bfad746cba
xinxilope <92169100+xinxilope@users.noreply.github.com>
2021-12-16 13:21:20 -0300

    Delete JavaScrypt1 directory

--- JavaScrypt1/02_tipos_variaveis.js
+++
@@ -1,9 +0,0 @@
-// declaração de variaveis
-
-
-const numero_constante = 10; //declarando uma constante
-let numero_let = 5; //declarando um let (scope somente no bloco)
-var numero_var = 2; //declarando uma variavel (scope global)
-var texto = "mateus"; // declarando uma var com string
-
-console.log("Meu nome é:",texto,"a constante é:",numero_constante,"a let é:",numero_let,"e a var é:",numero_var);

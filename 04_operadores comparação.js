a5023d589cd17c9b035f72c9e89369779212e4f8
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 09:09:57 -0300

    arquivo operadores comparacao

---
+++ 04_operadores comparação.js
@@ -0,0 +1,11 @@
+// uso do "=" na comparação
+
+const numero = 2;
+const texto = "2";
+
+console.log(typeof numero, typeof texto);
+console.log(numero === texto);
+
+// conversao explicita
+
+console.log(Number(texto) === numero);

2ee1d17d9db54fb47eafcbf2b05096717ec9eb3f
BuildTools <mateuseidt10@gmail.com>
2021-12-16 16:41:42 -0300

    introducao arrays

---
+++ JavaScript2/01_introducao_arrays.js
@@ -0,0 +1,16 @@
+//media de nota
+let nota1 = 10
+let nota2 = 7
+let nota3 = 5
+let nota4 = 8.5
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log(media)
+
+//media com a lista de notas
+let notas = [10,7,5,8.5]
+
+let media_notas = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media_notas)

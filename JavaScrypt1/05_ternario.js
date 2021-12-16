1106fd7657d71d8ab06fced4a67487270319a8f0
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 11:05:20 -0300

    mudei arquivos de local

---
+++ JavaScrypt1/05_ternario.js
@@ -0,0 +1,14 @@
+const minimo = 16;
+const real = 18;
+
+// if else
+
+if (real >= minimo) {
+    console.log("liberado")
+} else {
+    console.log("nao liberado")
+}
+
+// ou
+
+console.log(real >= minimo ? "liberado" : "nao liberado")

dd12b5013a6d6f3a9b0408eeb561fbbcec1d8a34
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 10:08:05 -0300

    uso de funcoes

---
+++ 07_funcoes.js
@@ -0,0 +1,20 @@
+//declarando a funcao com parametro
+
+function elevar2(num){
+    return num**2
+}
+
+num = 4 // definindo variavel/parametro
+console.log(elevar2(num)) //chamando a funcao
+
+
+/////////////////////////////////////////////////////
+
+
+//declarando funcao sem parametro
+
+function texto_padrao(){
+    return "ola, bem vindo!"
+}
+
+console.log(texto_padrao())

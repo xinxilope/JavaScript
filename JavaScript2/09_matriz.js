2eb365448176b893506297237d6711725a0fee6e
BuildTools <mateuseidt10@gmail.com>
2021-12-17 10:26:55 -0300

    usando for em matrizes

--- JavaScript2/09_matriz.js
+++ JavaScript2/09_matriz.js
@@ -1,11 +1,12 @@
+//declarando as 3 listas
 const notas1 = [10 , 6.5, 8 ,7.5]
 const notas2 = [9  , 6  , 6]
 const notas3 = [8.5, 9.5]
 
+//colocando as 3 listas em 1
 const notasGerais = [notas1,notas2,notas3]
 
 let media = 0
-
 for (let i = 0; i < notasGerais.length; i++){
     for (let j = 0; j < notasGerais[i].length; j++) {
         media += notasGerais[i][j]/notasGerais[i].length;

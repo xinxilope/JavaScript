a0bb11cf235076478153447fe50cef66b9b0508e
BuildTools <mateuseidt10@gmail.com>
2021-12-17 09:33:19 -0300

    calculando media com for

--- JavaScript2/07_for.js
+++ JavaScript2/07_for.js
@@ -1,5 +1,21 @@
+
+//estrutura padrao do for
+
 let numeros = [10,20,30,40,50]
 
 for (let i = 0; i < numeros.length; i++) {
     console.log(i,numeros[i])
 }
+
+//fazendo media de notas com for
+
+let notas = [7, 8, 6.5, 7.5]
+let soma_notas = 0
+
+for(let i = 0; i<notas.length; i++) {
+    soma_notas += notas[i];
+}
+
+let media = soma_notas/notas.length
+
+console.log(`a media das notas é ${media}`)

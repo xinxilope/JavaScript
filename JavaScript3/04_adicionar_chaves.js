8d37fcc703f3fac3ff85e164c535f2f853045045
BuildTools <mateuseidt10@gmail.com>
2021-12-20 09:29:06 -0300

    adicionando elementos no objeto

---
+++ JavaScript3/04_adicionar_chaves.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome:"mateus",
+    idade:18,
+    cpf:"12345678912",
+    email:"teste@email.com"
+}
+
+console.log(cliente)
+
+cliente.celular = "9238183" //criando chave e valor
+
+console.log(cliente)

46108950c4ed3ed12e12a2b2b382efe6274bb2b8
BuildTools <mateuseidt10@gmail.com>
2021-12-20 08:36:08 -0300

    introducao objetos

---
+++ JavaScript3/01_introducao_objetos.js
@@ -0,0 +1,11 @@
+const ListaCPF = ["13465417896","86739658158","48647251973"]
+
+const dados = ["nome","Andre","idade",36] //array
+
+//objeto
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:12345678912,
+    email:"teste@email.com"
+}

dffb8ab69d433ec0cea203850b3056608d2c717d
BuildTools <mateuseidt10@gmail.com>
2021-12-20 08:57:43 -0300

    printado informacoes do objeto

---
+++ JavaScript3/02_print_informacoes.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345678912",
+    email:"teste@email.com"
+}
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`)
+console.log(cliente.cpf.substring(0,3))
